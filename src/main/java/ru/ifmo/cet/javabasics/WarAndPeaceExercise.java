73ccda771630f2dee50d65bcd24474afc864d719
alexmichel1084 <39660411+alexmichel1084@users.noreply.github.com>
2018-10-24 20:16:30 +0300

    some homework Mihailov Alex task 2

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,63 @@
 package ru.ifmo.cet.javabasics;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.*;
+import java.io.File;
+
 
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
-        final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
-        final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
+    public static String warAndPeace() throws IOException {
+//считывание в мапу
+        Scanner in = new Scanner(new File("src/main/resources/WAP12.txt"));
+        HashMap<String,Integer> vocabulary= new HashMap<>();
+        readWords(in, vocabulary);
+        in = new Scanner(new File("src/main/resources/WAP34.txt"));
+        readWords(in, vocabulary);
+//удаление слов<10
+        for(Iterator<HashMap.Entry<String, Integer>> it=vocabulary.entrySet().iterator();it.hasNext();) {
+            HashMap.Entry<String, Integer> entry = it.next();
+            if (entry.getValue() < 10) {
+                it.remove();
+            }
+        }
+        ArrayList<String> res_array= new ArrayList<>();
+        while (!vocabulary.isEmpty()){
+            HashMap.Entry<String, Integer> max=vocabulary.entrySet().iterator().next();
+            for (Map.Entry<String, Integer> pair : vocabulary.entrySet()) {
+                if (max.getValue() < pair.getValue()) {
+                    max = pair;
+                } else if (max.getValue().equals(pair.getValue())) {
+                    if (max.getKey().compareTo(pair.getKey()) > 0) {
+                        max = pair;
+                    }
+                }
+            }
+            String string=max.getKey()+ " - "+max.getValue();
+            res_array.add(string);
+            vocabulary.remove(max.getKey());
+        }
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
-        throw new UnsupportedOperationException();
+        String res_string="";
+        for(String s:res_array) {
+            res_string+=s+"\n";
+        }
+        res_string = res_string.substring(0, res_string.length() - 1);
+        return res_string;
     }
-
+    public static void readWords(Scanner in, HashMap<String, Integer> vocabulary) {
+        while(in.hasNext()){
+            String[] words= (in.nextLine().toLowerCase().replaceAll("[^a-z\u0430-\u044f]", " ")).split(" ");
+            for (String word:words) {
+                if(word.length()<4) continue;
+                Integer value=vocabulary.get(word);
+                if (vocabulary.get(word) == null) {
+                    vocabulary.put(word,1);
+                }else{
+                    vocabulary.put(word,++value);
+                }
+            }
+        }
+    }
 }
